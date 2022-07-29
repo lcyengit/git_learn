82042d7c5ea7ad027767c82b1b93e3cd8a4e81bb
lcy <lcy@qq.com>
2022-07-29 22:25:57 +0800

    third

--- src/main/java/Test/gitte.java
+++ src/main/java/Test/gitte.java
@@ -4,5 +4,6 @@ public class gitte {
     public static void main(String[] args) {
         System.out.println("hello,git!!!");
         System.out.println("hello,git1");
+        System.out.println("hello,master");
     }
 }
